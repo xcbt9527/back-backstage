3a9af25123a2738d8b54847c71389092b968b43e
xcbt9527 <admin520>
2017-11-23 09:45:43 +0800

    接口调通

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    console.log(req.url);
+    next();
 });
-
+router.post('/login', (req, res, next) => {
+    console.log(1);
+    res.json({
+        code: '1',
+        msg: '操作成功'
+    });
+})
 module.exports = router;
