b26e14a39d216912d708b89bdb4891f9ae6adb9c
xcbt9527 <admin520>
2017-11-29 09:53:23 +0800

    新增浏览器缓存

--- app.ts
+++ app.ts
@@ -21,8 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
-
+app.use(express.static(path.join(__dirname, 'dist'),{maxAge:1000*60*60}));
 app.use(function (req, res, next) {
     if (req.url === '/api/user/login') {
         next();
